3bac1756720f22a1ea8a06f9dead02b001227866
DevTran2002 <92707826+DevTran2002@users.noreply.github.com>
2023-12-09 18:36:51 +0700

    Update DCT.py
    update code

--- pyproject_backup/tempt_code/DCT.py
+++ pyproject_backup/tempt_code/DCT.py
@@ -1,58 +1,61 @@
-from numpy import *
-import cv2
 import numpy as np
-import math
 from scipy.fftpack import dct, idct
-from PIL import Image
+import cv2
+import sys
 import heapq
 from collections import defaultdict
-import time
-import os
-import sys
-quantization_matrix = np.array([[16, 11, 10, 16, 24, 40, 51, 61],
-                                [12, 12, 14, 19, 26, 58, 60, 55],
-                                [14, 13, 16, 24, 40, 57, 69, 56],
-                                [14, 17, 22, 29, 51, 87, 80, 62],
-                                [18, 22, 37, 56, 68, 109, 103, 77],
-                                [24, 35, 55, 64, 81, 104, 113, 92],
-                                [49, 64, 78, 87, 103, 121, 120, 101],
-                                [72, 92, 95, 98, 112, 100, 103, 99]])
 
+def rgb_to_ycbcr(img):
+    r = img[:, :, 0]
+    g = img[:, :, 1]
+    b = img[:, :, 2]
+
+    y = 0.299 * r + 0.587 * g + 0.114 * b
+    cb = -0.1687 * r - 0.3313 * g + 0.5 * b + 128
+    cr = 0.5 * r - 0.4187 * g - 0.0813 * b + 128
+
+    return y, cb, cr
 
 def dct_2d(block):
-    result = np.zeros_like(block, dtype=float)
+    return dct(dct(block, axis=0, norm='ortho'), axis=1, norm='ortho')
 
-    for u in range(8):
-        for v in range(8):
-            cu = 1.0 / np.sqrt(2) if u == 0 else 1.0
-            cv = 1.0 / np.sqrt(2) if v == 0 else 1.0
+def idct_2d(block):
+    return idct(idct(block, axis=0, norm='ortho'), axis=1, norm='ortho')
 
-            sum_value = 0.0
-            for x in range(8):
-                for y in range(8):
-                    sum_value += block[x, y] * np.cos((2 * x + 1) * u * np.pi / 16) * np.cos((2 * y + 1) * v * np.pi / 16)
-
-            result[u, v] = 0.25 * cu * cv * sum_value
-
-    return result
-
-
-def idct_2d(dct_block):
-    result = np.zeros_like(dct_block, dtype=float)
-
-    for x in range(8):
-        for y in range(8):
-            sum_value = 0.0
-            for u in range(8):
-                for v in range(8):
-                    cu = 1.0 / np.sqrt(2) if u == 0 else 1.0
-                    cv = 1.0 / np.sqrt(2) if v == 0 else 1.0
-
-                    sum_value += cu * cv * dct_block[u, v] * np.cos((2 * x + 1) * u * np.pi / 16) * np.cos((2 * y + 1) * v * np.pi / 16)
-
-            result[x, y] = 0.25 * sum_value
-
-    return result
+# def dct_2d(block):
+#     result = np.zeros_like(block, dtype=float)
+#
+#     for u in range(8):
+#         for v in range(8):
+#             cu = 1.0 / np.sqrt(2) if u == 0 else 1.0
+#             cv = 1.0 / np.sqrt(2) if v == 0 else 1.0
+#
+#             sum_value = 0.0
+#             for x in range(8):
+#                 for y in range(8):
+#                     sum_value += block[x, y] * np.cos((2 * x + 1) * u * np.pi / 16) * np.cos((2 * y + 1) * v * np.pi / 16)
+#
+#             result[u, v] = 0.25 * cu * cv * sum_value
+#
+#     return result
+#
+#
+# def idct_2d(dct_block):
+#     result = np.zeros_like(dct_block, dtype=float)
+#
+#     for x in range(8):
+#         for y in range(8):
+#             sum_value = 0.0
+#             for u in range(8):
+#                 for v in range(8):
+#                     cu = 1.0 / np.sqrt(2) if u == 0 else 1.0
+#                     cv = 1.0 / np.sqrt(2) if v == 0 else 1.0
+#
+#                     sum_value += cu * cv * dct_block[u, v] * np.cos((2 * x + 1) * u * np.pi / 16) * np.cos((2 * y + 1) * v * np.pi / 16)
+#
+#             result[x, y] = 0.25 * sum_value
+#
+#     return result
 
 def resize_with_padding(image, block_size):
     # Lấy kích thước ảnh
@@ -63,56 +66,36 @@ def resize_with_padding(image, block_size):
     padding_width = (width // block_size + 1) * block_size - width
 
     # Thêm padding vào ảnh
-    padded_image = np.pad(image, ((0, padding_height), (0, padding_width), (0, 0)), mode='constant')
+    if(padding_height != 0 and padding_width !=0):
+        padded_image = np.pad(image, ((0, padding_height), (0, padding_width), (0, 0)), mode='constant')
 
     return padded_image
 
 
+def apply_dct(img):
+    block_size = 8
+    height, width, _ = img.shape
 
+    for i in range(0, height, block_size):
+        for j in range(0, width, block_size):
+            block = img[i:i+block_size, j:j+block_size, :]
+            y, cb, cr = rgb_to_ycbcr(block)
 
-def split_into_macroblocks(image, block_size):
-    # Kích thước của ảnh
-    image_height, image_width, _ = image.shape
+            y_dct = dct_2d(y)
+            cb_dct = dct_2d(cb)
+            cr_dct = dct_2d(cr)
 
-    # Số lượng macroblocks theo chiều cao và chiều rộng
-    num_macroblocks_height = image_height // block_size
-    num_macroblocks_width = image_width // block_size
+            # # Perform quantization here (not shown in this example)
+            # y_idct = idct_2d(y_dct)
+            # cb_idct = idct_2d(cb_dct)
+            # cr_idct = idct_2d(cr_dct)
+            #
+            # Update the original block with the inverse DCT results
+            img[i:i+block_size, j:j+block_size, 0] = y_dct
+            img[i:i+block_size, j:j+block_size, 1] = cb_dct
+            img[i:i+block_size, j:j+block_size, 2] = cr_dct
 
-    # Tách ảnh thành các macroblock 8x8
-    macroblocks = []
-    for i in range(num_macroblocks_height):
-        for j in range(num_macroblocks_width):
-            macroblock = image[i * block_size: (i + 1) * block_size,
-                         j * block_size: (j + 1) * block_size, :]
-            macroblocks.append(macroblock)
-
-    # Chuyển đổi danh sách các macroblocks thành một mảng NumPy
-    macroblocks_array = np.array(macroblocks)
-
-    return macroblocks_array
-
-def combine_macroblocks(macroblocks, image_shape):
-    image_height, image_width, _ = image_shape
-    macroblock_size = macroblocks.shape[1]
-    num_macroblocks_height = image_height // macroblock_size
-    num_macroblocks_width = image_width // macroblock_size
-
-    combined_image = np.zeros(image_shape, dtype=np.uint8)
-
-    for i in range(num_macroblocks_height):
-        for j in range(num_macroblocks_width):
-            combined_image[i * macroblock_size : (i + 1) * macroblock_size,
-                           j * macroblock_size : (j + 1) * macroblock_size, :] = macroblocks[i * num_macroblocks_width + j]
-
-    return combined_image
-
-
-def quantize_macroblocks(macroblocks, quantization_factor):
-    return (macroblocks // quantization_factor).astype(np.uint8) * quantization_factor
-
-# Hàm để giải lượng tử hóa các macroblocks
-def dequantize_macroblocks(quantized_macroblocks, quantization_factor):
-    return quantized_macroblocks * quantization_factor
+    return img
 
 def run_length_encode(data):
     encoded_data = []
@@ -190,6 +173,8 @@ def huffman_decode(encoded_data, huffman_tree):
 
     return decoded_data
 
+
+
 def loadVideo():
 
     cap = cv2.VideoCapture(r"C:\Users\Public\Documents\PythonCode\myvideo.mjpeg")
@@ -204,70 +189,43 @@ def loadVideo():
         ret, frame = cap.read()
 
         if ret == True:
-
             # cv2.imshow('Frame', frame)
-            compress(frame)
+            loadImg(frame)
 
             # Bấm q để thoát
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 break
-
-
         else:
             break
-
-
     cap.release()
 
     # đóng hết các frame
     cv2.destroyAllWindows()
 
+def loadImg(img):
+    # Load an RGB image
+    block_size = 8
+    # image_path = 'hinhhoc.jpg'
+    # _image = cv2.imread(image_path)
+    _image =img
+    original_image = resize_with_padding(_image, block_size)
+    print(original_image.shape)
 
-def compress(img):
-
-
-    real_time = time.time()
-    mb_size = 8
-    # Thực hiện resize với padding
-    resized_image = resize_with_padding(img, mb_size)
-    # /////////////////////////////////////////////////////
-    # Chuyển đổi ảnh thành kiểu dữ liệu float32
-    image_float = resized_image.astype(np.float32)
-    # image_float = resized_image
-    compressed_size1 = sys.getsizeof(image_float)
-    print(compressed_size1)
-
-    # Tách các kênh màu
-    red_channel = resized_image[:, :, 0]
-    green_channel = resized_image[:, :, 1]
-    blue_channel = resized_image[:, :, 2]
-    # Áp dụng DCT cho từng kênh màu
-    red_dct = dct(dct(red_channel, axis=0, norm='ortho'), axis=1, norm='ortho')
-    green_dct = dct(dct(green_channel, axis=0, norm='ortho'), axis=1, norm='ortho')
-    blue_dct = dct(dct(blue_channel, axis=0, norm='ortho'), axis=1, norm='ortho')
-
-    # Gộp các kênh màu để có ảnh ban đầu
-    reconstructed_image = np.stack([red_dct, green_dct, blue_dct], axis=2).clip(0, 255).astype(np.uint8)
-    macroblocks = split_into_macroblocks(reconstructed_image, mb_size)
-
-    for i in range(macroblocks.shape[0]):
-        # Lượng tử hóa các macroblocks
-        quantization_factor = 2
-        quantized_macroblocks = quantize_macroblocks(macroblocks, quantization_factor)
+    # Apply DCT to the image
+    compressed_image = apply_dct(original_image)
 
     # Mã hóa độ dài  (RLC) cho từng macroblock
-    rle_encoded_macroblocks = [run_length_encode(macroblock.flatten()) for macroblock in quantized_macroblocks]
-
-    # # Hiển thị thông tin về dữ liệu đã mã hóa RLE
+    rle_encoded_macroblocks = [run_length_encode(macroblock.flatten()) for macroblock in compressed_image]
+    # Hiển thị thông tin về dữ liệu đã mã hóa RLE
     # for i, rle_data in enumerate(rle_encoded_macroblocks):
     #     print(f"RLE Encoded Data for Macroblock {i + 1}:", rle_data)
+
 
     # Tính toán tần số xuất hiện từ dữ liệu RLE
     frequencies = defaultdict(int)
     for rle_data in rle_encoded_macroblocks:
         for symbol, count in rle_data:
             frequencies[symbol] += count
-
 
     # Xây dựng cây Huffman từ tần số xuất hiện
     huffman_tree = build_huffman_tree(frequencies)
@@ -283,26 +241,12 @@ def compress(img):
         [huffman_mapping[symbol] for symbol, _ in rle_data]
         for rle_data in rle_encoded_macroblocks
     ]
-    real_time1 = time.time()
 
-    print(real_time1-real_time)
+    exit()
 
-
-    compressed_size = sys.getsizeof(huffman_encoded_macroblocks)
-    compressed_size_2 = sys.getsizeof(rle_encoded_macroblocks)
-    print(rle_encoded_macroblocks)
-    for i in range(macroblocks.shape[0]):
-        # Giải lượng tử hóa các macroblocks
-        dequantized_macroblocks = dequantize_macroblocks(quantized_macroblocks, quantization_factor)
-
-    # # Áp dụng ngược DCT để khôi phục ảnh ban đầu từ các kênh đã áp dụng DCT
-    # red_idct = idct(idct(red_dct, axis=0, norm='ortho'), axis=1, norm='ortho')
-    # green_idct = idct(idct(green_dct, axis=0, norm='ortho'), axis=1, norm='ortho')
-    # blue_idct = idct(idct(blue_dct, axis=0, norm='ortho'), axis=1, norm='ortho')
-
-    # Gộp các kênh màu để có ảnh ban đầu
-    # reconstructed_image = np.stack([red_idct, green_idct, blue_idct], axis=2).clip(0, 255).astype(np.uint8)
+    # cv2.imshow('Compressed Image', compressed_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 loadVideo()
-
